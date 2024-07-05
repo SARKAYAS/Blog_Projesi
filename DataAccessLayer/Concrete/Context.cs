49da93cd411339892bef34852eccd31fbd0cbeae
Aysegul <Aysegul@SARKAYAS>
2024-07-06 01:15:47 +0300

    Proje dosyası ekle.

---
+++ DataAccessLayer/Concrete/Context.cs
@@ -0,0 +1,37 @@
+﻿using EntityLayer.Concrete;
+using System;
+using System.Collections.Generic;
+using Microsoft.EntityFrameworkCore;
+using System.Data.Entity;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using DbContext = Microsoft.EntityFrameworkCore.DbContext;
+using System.Data.SqlClient;
+
+namespace DataAccessLayer.Concrete
+{
+    public class Context : DbContext
+    {
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            optionsBuilder.UseSqlServer(@"Data Source=AYSEGUL;Initial Catalog=Blog;Integrated Security=True;Connect Timeout=30;Encrypt=True;TrustServerCertificate=True;ApplicationIntent=ReadWrite;MultiSubnetFailover=False");
+
+           // optionsBuilder.UseSqlServer(@"Data Source=AYSEGUL;Initial Catalog=Blog;Integrated Security=True;Connect Timeout=30;Encrypt=True");
+
+
+            //string connectionString = "Server=AYSEGUL;Database=Blog;User Id=saa;Password=123456789;TrustServerCertificate=true;";
+            //SqlConnection connection = new SqlConnection(connectionString);
+
+        }
+
+        public Microsoft.EntityFrameworkCore.DbSet<About> Abouts { get; set; }
+        public Microsoft.EntityFrameworkCore.DbSet<Admin> Admins { get; set; }
+        public Microsoft.EntityFrameworkCore.DbSet<Author> Author { get; set; }
+        public Microsoft.EntityFrameworkCore.DbSet<Blog> Blogs { get; set; }
+        public Microsoft.EntityFrameworkCore.DbSet<Category> Categories { get; set; }
+        public Microsoft.EntityFrameworkCore.DbSet<Contact> Contact { get; set; }
+        public Microsoft.EntityFrameworkCore.DbSet<Comment> Comment { get; set; }
+        public Microsoft.EntityFrameworkCore.DbSet<SubscribeMail> SubscribeMails { get; set; }
+    }
+}
---
+++ DataAccessLayer/Concrete/Repository.cs
@@ -0,0 +1,79 @@
+﻿using Microsoft.EntityFrameworkCore;
+using DataAccessLayer.Abstract;
+using System;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.Linq;
+using System.Linq.Expressions;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DataAccessLayer.Concrete
+{
+    public class Repository<T> : IRepository<T> where T : class
+    {
+        Context c = new Context();
+
+        Microsoft.EntityFrameworkCore.DbSet<T> _object;
+        //Degisiklik yapmadan onceki hali:  DbSet<T> _object;
+
+
+        public Repository()
+        {
+            _object = c.Set<T>();
+        }
+
+        public void Delete(T p)
+        {
+            c.Remove(p);
+            c.SaveChanges();
+        }
+
+        public T Find(Expression<Func<T, bool>> where)
+        {
+            return _object.FirstOrDefault(where);
+        }
+
+        public T GetByID(int id)
+        {
+            return _object.Find(id);
+        }
+
+        public void Insert(T p)
+        {
+             c.Add(p);
+             c.SaveChanges();
+        }
+
+        public List<T> List()
+        {
+            //return _object?.ToList() ?? new List<T>();
+
+            //return _object.ToList();
+          
+                try
+                {
+                    return _object?.ToList() ?? new List<T>();
+                }
+                catch (System.InvalidOperationException ex)
+                {
+                    // Hata yönetimi burada
+                    // Loglama veya başka bir işlem yapabilirsiniz.
+                    return new List<T>();
+                }
+            
+
+        }
+
+        public List<T> List(Expression<Func<T, bool>> where)
+        {
+            return _object.Where(where).ToList();
+        }
+
+        public void Update(T p)
+        {
+            c.Update(p);
+             c.SaveChanges();
+        }
+    }
+}
